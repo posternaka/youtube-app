02dd5d666e4427ef667997177853062453fd6ce5
posternaka <nik.petrenko97@gmail.com>
2022-11-10 23:52:47 +0300

    adding style for auth page

--- src/pages/Authorization.tsx
+++ src/pages/Authorization.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import '../styles/style.scss';
 
 const Authorization = () => {
   return (
-    <div>Authorization</div>
+    <div className='login'>
+      <h2>Login</h2>
+      <form>
+        <div className='user'>
+          <input type="text" name="" required/>
+          <label>Username</label>
+        </div>
+        <div className='user'>
+          <input type="password" name="" required/>
+          <label>Password</label>
+        </div>
+        <div className='button-form'>
+          <button id="submit">Submit</button>
+          <div id="register">
+            Don't have an account?
+            <button>Register</button>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
